e5a9339c6b778e81ea5aa6b0b95c7787efe569d5
zhaozhifeng <zhifeng.zhao@mobiiot.com.cn>
2024-03-08 19:25:19 +0800

    backgroundTask

--- app/src/main/java/com/rhythmcoderzzf/androidstudy/background/BackgroundWorkMainActivity.java
+++ app/src/main/java/com/rhythmcoderzzf/androidstudy/background/BackgroundWorkMainActivity.java
@@ -1,26 +1,30 @@
 package com.rhythmcoderzzf.androidstudy.background;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import com.rhythmcoderzzf.androidstudy.R;
+import com.rhythmcoderzzf.androidstudy.background.alarm.BackgroundAlarmActivity;
+import com.rhythmcoderzzf.androidstudy.background.service.BackgroundServiceActivity;
 import com.rhythmcoderzzf.baselib.BaseActivity;
+import com.rhythmcoderzzf.baselib.ListAdapter;
+import com.rhythmcoderzzf.baselib.TitleBean;
 
-public class BackgroundWorkMainActivity extends BaseActivity implements View.OnClickListener {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BackgroundWorkMainActivity extends BaseActivity {
+    private List<TitleBean> initRvDataList() {
+        List<TitleBean> list = new ArrayList<>();
+        list.add(new TitleBean(getString(R.string.background_service), BackgroundServiceActivity.class));
+        list.add(new TitleBean(getString(R.string.background_alarm), BackgroundAlarmActivity.class));
+        return list;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_background_work);
-        findViewById(R.id.btn_alarm).setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.btn_alarm) {
-            startService(new Intent(this, AlarmManagerCaseService.class));
-        }
+        setContentView(R.layout.activity_rv);
+        initSimpleProjectListView(findViewById(R.id.rv), new ListAdapter(initRvDataList()));
     }
 
 }
--- app/src/main/java/com/rhythmcoderzzf/androidstudy/background/alarm/AlarmManagerCaseService.java
+++ app/src/main/java/com/rhythmcoderzzf/androidstudy/background/alarm/AlarmManagerCaseService.java
@@ -1,12 +1,16 @@
-package com.rhythmcoderzzf.androidstudy.background;
+package com.rhythmcoderzzf.androidstudy.background.alarm;
 
+import android.annotation.SuppressLint;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.Environment;
 import android.os.IBinder;
+import android.os.PowerManager;
 import android.os.SystemClock;
+
+import androidx.annotation.Nullable;
 
 import com.rhythmcoderzzf.baselib.cmd.CmdUtil;
 
@@ -20,16 +24,19 @@ import java.util.Date;
 public class AlarmManagerCaseService extends Service {
     private static final String TAG = "AlarmManagerCaseService";
     private File mFile;
+    private PowerManager.WakeLock mWakelock;
 
+    @SuppressLint("InvalidWakeLockTag")
     @Override
     public void onCreate() {
         super.onCreate();
+        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        mWakelock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, TAG);
         mFile = new File(getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS), "alarm_log.txt");
         if (mFile.exists()) {
             CmdUtil.d(TAG, "file already exists");
             mFile.delete();
         }
-
         try {
             boolean isCreate = mFile.createNewFile();
             if (isCreate) {
@@ -44,39 +51,39 @@ public class AlarmManagerCaseService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        netWorkIsEnable();
+        mWakelock.acquire();
+        doBackGroundWork();
         AlarmManager manager = (AlarmManager) getSystemService(ALARM_SERVICE);
         int anHour = 60 * 1000;
         long triggerAtTime = SystemClock.elapsedRealtime() + anHour;
         Intent i = new Intent(this, AlarmManagerCaseService.class);
-        PendingIntent pi = PendingIntent.getService(this, 0, i, PendingIntent.FLAG_IMMUTABLE);
+        PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
         manager.setExactAndAllowWhileIdle(AlarmManager.ELAPSED_REALTIME_WAKEUP, triggerAtTime, pi);
         return super.onStartCommand(intent, flags, startId);
     }
 
-    public void netWorkIsEnable() {
-        int status = -1;
-
-        try {
-            // 通过ping百度检测网络是否可用
-            Process p = Runtime.getRuntime().exec("/system/bin/ping -c " + 1 + " 202.108.22.5");
-            status = p.waitFor(); // 只有0时表示正常返回
-        } catch (IOException e) {
-            CmdUtil.e(TAG, "MyService IOException--" + e.getMessage());
-            status = -2;
-            e.printStackTrace();
-        } catch (InterruptedException e) {
-            status = -3;
-            CmdUtil.e(TAG, "MyService InterruptedException--" + e.getMessage());
-            e.printStackTrace();
-        }
+    public void doBackGroundWork() {
         if (mFile.exists()) {
+            int status = -1;
+            try {
+                // 通过ping百度检测网络是否可用
+                Process p = Runtime.getRuntime().exec("/system/bin/ping -c " + 1 + " 202.108.22.5");
+                status = p.waitFor(); // 只有0时表示正常返回
+            } catch (IOException e) {
+                CmdUtil.e(TAG, "MyService IOException--" + e.getMessage());
+                status = -2;
+                e.printStackTrace();
+            } catch (InterruptedException e) {
+                status = -3;
+                CmdUtil.e(TAG, "MyService InterruptedException--" + e.getMessage());
+                e.printStackTrace();
+            }
             try {
                 BufferedWriter writer = new BufferedWriter(new FileWriter(mFile, true));
                 Date date = new Date();
                 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
-                String currentDateTime = sdf.format(date) + "--status--" + status;
-                CmdUtil.d(TAG, currentDateTime);
+                String currentDateTime = sdf.format(date);
+                CmdUtil.d(TAG, currentDateTime + "--" + status);
                 writer.write(currentDateTime);
                 writer.newLine();
                 writer.close();
@@ -89,9 +96,15 @@ public class AlarmManagerCaseService extends Service {
 
     }
 
+    @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mWakelock.release();
     }
 }
