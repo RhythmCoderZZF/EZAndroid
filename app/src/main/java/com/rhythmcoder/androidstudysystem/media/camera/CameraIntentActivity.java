b162e4c453708eafacb1f572f51e3bc6799a7dd5
zzf <rhtyhmcoderzzf@gmail.com>
2024-11-05 21:05:25 +0800

    camera

--- app/src/main/java/com/rhythmcoder/androidstudysystem/media/camera/CameraIntentActivity.java
+++ app/src/main/java/com/rhythmcoder/androidstudysystem/media/camera/CameraIntentActivity.java
@@ -1,24 +1,82 @@
 package com.rhythmcoder.androidstudysystem.media.camera;
 
+import android.Manifest;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.GridView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import com.rhythmcoder.androidstudysystem.R;
 import com.rhythmcoder.baselib.BaseActivity;
 import com.rhythmcoder.baselib.utils.LogUtil;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 public class CameraIntentActivity extends BaseActivity implements View.OnClickListener {
     static final int REQUEST_IMAGE_CAPTURE = 1;
     static final int REQUEST_VIDEO_CAPTURE = 2;
+    private TextureView textureView;
+    private CameraDevice cameraDevice;
+    private String cameraId = "0";
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            cameraDevice = camera;
+            startPreview();
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            camera.close();
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            camera.close();
+            cameraDevice = null;
+        }
+    };
+    private final TextureView.SurfaceTextureListener surfaceTextureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surfaceTexture, int w, int h) {
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            closeCamera();
+            return true;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +85,18 @@ public class CameraIntentActivity extends BaseActivity implements View.OnClickLi
         findViewById(R.id.btn_lunch_camera).setOnClickListener(this);
         findViewById(R.id.btn_lunch_video).setOnClickListener(this);
         ((TextView) findViewById(R.id.tv_camera_id)).append(getCameraCount());
+        textureView = findViewById(R.id.texture);
+        ((TextView) findViewById(R.id.edt_camera_id)).setOnEditorActionListener((v, actionId, event) -> {
+            LogUtil.d(TAG, "actionId:" + actionId + " keyEvent:" + event);
+            if (0 == actionId && event.getAction()==KeyEvent.ACTION_DOWN) {
+                cameraId = v.getText().toString();
+                textureView.setSurfaceTextureListener(surfaceTextureListener);
+                openCamera();
+                return true;
+            }
+            return false;
+        });
+
     }
 
     @Override
@@ -52,6 +122,61 @@ public class CameraIntentActivity extends BaseActivity implements View.OnClickLi
         }
     }
 
+    private void openCamera() {
+        CameraManager cameraManager = (CameraManager) getSystemService(CAMERA_SERVICE);
+        try {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
+                return;
+            }
+            cameraManager.openCamera(cameraId, stateCallback, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void startPreview() {
+        try {
+            SurfaceTexture texture = textureView.getSurfaceTexture();
+            texture.setDefaultBufferSize(1920, 1080); // 设置预览分辨率
+
+            Surface surface = new Surface(texture);
+            CaptureRequest.Builder captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            captureRequestBuilder.addTarget(surface);
+
+            cameraDevice.createCaptureSession(
+                    Collections.singletonList(surface),
+                    new CameraCaptureSession.StateCallback() {
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession session) {
+                            try {
+                                session.setRepeatingRequest(captureRequestBuilder.build(), null, null);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                        }
+                    }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        closeCamera();
+    }
 
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
