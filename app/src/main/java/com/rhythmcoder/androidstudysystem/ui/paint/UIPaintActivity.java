1f76726d83937fb829324c65a3197a52f4dbaf17
zzf <rhtyhmcoderzzf@gmail.com>
2024-11-24 21:53:54 +0800

    update gradle to 8.7

--- app/src/main/java/com/rhythmcoder/androidstudysystem/ui/paint/UIPaintActivity.java
+++ app/src/main/java/com/rhythmcoder/androidstudysystem/ui/paint/UIPaintActivity.java
@@ -2,9 +2,15 @@ package com.rhythmcoder.androidstudysystem.ui.paint;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 
 import com.rhythmcoder.androidstudysystem.R;
@@ -12,7 +18,7 @@ import com.rhythmcoder.baselib.BaseActivity;
 
 
 public class UIPaintActivity extends BaseActivity implements View.OnClickListener {
-
+    private Bitmap bitmap = Bitmap.createBitmap(100, 35, Bitmap.Config.ARGB_8888);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +26,31 @@ public class UIPaintActivity extends BaseActivity implements View.OnClickListene
         findViewById(R.id.btn_test).setOnClickListener(this);
         findViewById(R.id.btn_portrait).setOnClickListener(this);
         findViewById(R.id.btn_land).setOnClickListener(this);
+
+        bitmap.eraseColor(Color.parseColor("#FFFFFFFF"));
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+        paint.setTextSize(24f);
+        paint.setColor(Color.BLACK);
+        canvas.drawText("O", 5, 29, paint);
+        ((ImageView) findViewById(R.id.iv_test)).setImageBitmap(bitmap);
+        int[] pix = new int[bitmap.getWidth() * bitmap.getHeight()];
+        calculateSingleColor(bitmap, pix, 0, bitmap.getHeight(), Color.WHITE);
+    }
+
+    private static void calculateSingleColor(Bitmap bitmap, int[] pix, int startRow, int endRow, int bgColor) {
+        for (int y = startRow; y < endRow; y++) {
+            for (int x = 0; x < bitmap.getWidth(); x++) {
+                int index = y * bitmap.getWidth() + x;
+                int originColor = bitmap.getPixel(x, y);
+                if (originColor != Color.WHITE && originColor != Color.BLACK) {
+                    Log.d("zzf", "color:" + originColor);
+                } else {
+                    Log.d("zzf", "white or black:" + originColor);
+                }
+                pix[index] = ( originColor== bgColor) ? bgColor : Color.BLACK;
+            }
+        }
     }
 
     @Override
