caf91361171253aaeb5566e51501e576b931e5df
zzf <rhtyhmcoderzzf@gmail.com>
2025-07-08 23:18:00 +0800

    更新EZWifi

--- app/src/main/java/com/rhythmcoderzzf/androidstudysystem/wifi/WlanScanActivity.java
+++ app/src/main/java/com/rhythmcoderzzf/androidstudysystem/wifi/WlanScanActivity.java
@@ -1,142 +1,78 @@
 package com.rhythmcoderzzf.androidstudysystem.wifi;
 
 import android.Manifest;
-import android.app.AlertDialog;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-import android.content.pm.PackageManager;
+import android.annotation.SuppressLint;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import com.rhythmcoderzzf.androidstudysystem.R;
+import com.rhythmcoderzzf.androidstudysystem.databinding.ActivityWifiWlanScanBinding;
 import com.rhythmcoderzzf.baselib.BaseActivity;
+import com.rhythmcoderzzf.ezandroid.EZPermission;
 import com.rhythmcoderzzf.ezandroid.connection.EZWifi;
 
 import java.util.List;
 
-public class WlanScanActivity extends BaseActivity implements View.OnClickListener {
-    private TextView mTv;
-    private WifiManager mWifiManager;
-    private String[] mPermissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
-
-    private TextView tvConnectedSSID;
-    private EZWifi mEZWifiManager;
+@SuppressLint("MissingPermission")
+public class WlanScanActivity extends BaseActivity<ActivityWifiWlanScanBinding> implements View.OnClickListener {
+    private EZWifi mEZWifi;
 
     private EZWifi.Callback mCallback = new EZWifi.Callback() {
         @Override
-        protected void onWifiConnected(WifiInfo info, String ssid) {
-            updateTvConnectedSSID(info, ssid);
+        protected void onWifiConnected(WifiInfo info) {
+            updateTvConnectedSSID(info);
         }
-    };
 
-    //1.系统会在完成扫描请求时调用此监听器，提供其成功/失败状态。
-    private BroadcastReceiver mReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            boolean success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
-            if (success) {
-                scanSuccess();
-            } else {
-                // scan failure handling
-                scanFailure();
-            }
+        protected void onScanResult(List<ScanResult> scanResults) {
+            updateScanResult(scanResults);
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wifi_wlan_scan);
-        mTv = findViewById(R.id.tv_result);
-        findViewById(R.id.btn_scan).setOnClickListener(this);
-        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        IntentFilter intentFilter = new IntentFilter();
-        intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        registerReceiver(mReceiver, intentFilter);
+        mBinding.btnScan.setOnClickListener(this);
+        mEZWifi = EZWifi.getInstance(this, mCallback).registerReceiver(WifiManager.NETWORK_STATE_CHANGED_ACTION
+                , WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+    }
 
-        tvConnectedSSID = findViewById(R.id.tv_ssid);
-        mEZWifiManager = EZWifi.getInstance(this, mCallback).register(this);
+    @Override
+    protected ActivityWifiWlanScanBinding inflateViewBinding(@NonNull LayoutInflater layoutInflater) {
+        return ActivityWifiWlanScanBinding.inflate(layoutInflater);
     }
 
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.btn_scan) {
-            if (ContextCompat.checkSelfPermission(this, mPermissions[0]) == PackageManager.PERMISSION_GRANTED) {
-                boolean success = mWifiManager.startScan();
-                if (!success) {
-                    scanFailure();
+            new EZPermission(this).requestPermission(new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                    , Manifest.permission.CHANGE_WIFI_STATE}, (grant) -> {
+                if (!mEZWifi.startScan()) {
+                   toast("请打开Wifi");
                 }
-            } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, mPermissions[0])) {
-                showInContextUI();
-            } else {
-                requestPermissions(mPermissions, 0);
-            }
-        }
-    }
-
-    private void scanFailure() {
-        toast("扫描失败");
-    }
-
-    private void scanSuccess() {
-        List<ScanResult> results = mWifiManager.getScanResults();
-        mTv.setText("");
-        results.forEach(scanResult -> {
-            mTv.append("ssid:" + scanResult.SSID + " level:" + scanResult.level + " frequency:" + scanResult.frequency + "\n");
-        });
-    }
-
-    private void showInContextUI() {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
-        dialog.setTitle("Attention");
-        dialog.setMessage("We need permission to Continue the action or workflow in this app !!");
-        dialog.setNegativeButton("cancel", (d, which) -> {
-            d.dismiss();
-        });
-        dialog.setPositiveButton("ok", (dialog1, which) -> {
-            dialog1.dismiss();
-            requestPermissions(mPermissions, 0);
-
-        });
-        dialog.show();
-    }
-
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 0:
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    toast("已开启相关权限");
-                } else {
-                    if (shouldShowRequestPermissionRationale(permissions[0])) {
-                        toast("需要开启相关权限才能使用该功能哦");
-                    } else {
-                        toast("权限被禁用");
-                    }
-                }
+            });
         }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        unregisterReceiver(mReceiver);
+        mEZWifi.unRegisterReceiver();
     }
 
-    private void updateTvConnectedSSID(WifiInfo info, String ssid) {
-        tvConnectedSSID.setText(ssid);
+    private void updateTvConnectedSSID(WifiInfo info) {
+        mBinding.tvSsid.setText(info.getSSID());
+    }
+
+    private void updateScanResult(List<ScanResult> scanResults) {
+        mBinding.tvResult.setText("");
+        scanResults.forEach(scanResult -> {
+            mBinding.tvResult.append("ssid:" + scanResult.SSID + " level:" + scanResult.level + " frequency:" + scanResult.frequency + "\n");
+        });
     }
 }
--- ezandroid/src/main/java/com/rhythmcoderzzf/ezandroid/connection/EZWifi.java
+++ ezandroid/src/main/java/com/rhythmcoderzzf/ezandroid/connection/EZWifi.java
@@ -6,21 +6,19 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.NetworkInfo;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
-import androidx.annotation.NonNull;
-import androidx.lifecycle.DefaultLifecycleObserver;
-import androidx.lifecycle.LifecycleOwner;
-
 import com.rhythmcoderzzf.ezandroid.utils.EZLogUtil;
 
-import java.util.Objects;
+import java.util.List;
 
 /**
- * wifi模块相关
+ * wifi模块相关,需要权限——Manifest.permission.ACCESS_WIFI_STATE;ACCESS_FINE_LOCATION
  */
-public class EZWifi implements DefaultLifecycleObserver {
+@SuppressLint("MissingPermission")
+public class EZWifi {
     private static final String TAG = EZWifi.class.getSimpleName();
     public Context mContext;
     private final WifiChangeReceiver mWifiReceiver;
@@ -31,12 +29,25 @@ public class EZWifi implements DefaultLifecycleObserver {
         @Override
         public void onReceive(Context context, Intent intent) {
             EZLogUtil.v(TAG, "receive broadcast intent action:" + intent.getAction());
-            if (Objects.equals(intent.getAction(), WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+            String action = intent.getAction();
+            if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(action)) {
                 NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
                 if (networkInfo != null && networkInfo.isConnected()) {
                     if (mCallback != null) {
-                        mCallback.onWifiConnected(mWifiManager.getConnectionInfo(), getCurrentConnectedSSID());
+                        mCallback.onWifiConnected(mWifiManager.getConnectionInfo());
                     }
+                }
+            } else if (WifiManager.SCAN_RESULTS_AVAILABLE_ACTION.equals(action)) {
+                boolean success = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (!success) {
+                    return;
+                }
+                List<ScanResult> scanResults = mWifiManager.getScanResults();
+                if (scanResults == null || scanResults.isEmpty()) {
+                    return;
+                }
+                if (mCallback != null) {
+                    mCallback.onScanResult(scanResults);
                 }
             }
         }
@@ -53,42 +64,49 @@ public class EZWifi implements DefaultLifecycleObserver {
         return new EZWifi(context, callback);
     }
 
-    public EZWifi register(LifecycleOwner lifecycleOwner) {
-        lifecycleOwner.getLifecycle().addObserver(this);
+    public EZWifi registerReceiver(String... actions) {
+        IntentFilter intentFilter = new IntentFilter();
+        for (String action : actions) {
+            intentFilter.addAction(action);
+        }
+        mContext.registerReceiver(mWifiReceiver, intentFilter);
         return this;
     }
 
-    @Override
-    public final void onCreate(@NonNull LifecycleOwner owner) {
-        IntentFilter intentFilter = new IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION);
-        mContext.registerReceiver(mWifiReceiver, intentFilter);
-    }
-
-    @Override
-    public final void onDestroy(@NonNull LifecycleOwner owner) {
-        mContext.unregisterReceiver(mWifiReceiver);
-        owner.getLifecycle().removeObserver(this);
+    public EZWifi unRegisterReceiver() {
+        if (mWifiReceiver != null) mContext.unregisterReceiver(mWifiReceiver);
+        return this;
     }
 
     /**
-     * 获取当前连接的wifi ssid
-     *
-     * @return wifi ssid
+     * 扫描附近WiFi
      */
-    public String getCurrentConnectedSSID() {
-        WifiInfo wifiInfo = mWifiManager.getConnectionInfo();
-        String ssid = wifiInfo.getSSID();
-        if (ssid.startsWith("\"") && ssid.endsWith("\"")) {
-            ssid = ssid.substring(1, ssid.length() - 1);
+    public boolean startScan() {
+        if (mWifiManager != null && mWifiManager.isWifiEnabled()) {
+            mWifiManager.startScan();
+            return true;
         }
-        if (ssid.equalsIgnoreCase("<unknown ssid>")) {
-            ssid = "";
-        }
-        return ssid;
+        return false;
     }
 
+    /**
+     * 回调类
+     */
     public static class Callback {
-        protected void onWifiConnected(WifiInfo info, String ssid) {
+        /**
+         * 当WIFI连接成功回调
+         *
+         * @param info 已连接的wifi。例如""wifi-01"",如果为空则返回""<unknown ssid>""。注意getSsid()获取的WiFi名带双引号
+         */
+        protected void onWifiConnected(WifiInfo info) {
+        }
+
+        /**
+         * 扫描附近的wifi列表
+         * @param scanResults 附近的wifi列表。注意getWifiSsid()获取的WiFi名没有双引号
+         */
+        protected void onScanResult(List<ScanResult> scanResults) {
+
         }
     }
 }
